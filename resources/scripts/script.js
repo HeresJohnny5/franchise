bf0ccee95fb2869bf95ffc2acc74625580fa758a
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-03-19 20:30:00 -0400

    add script to session zip code. Add form into modal.

---
+++ resources/scripts/script.js
@@ -0,0 +1,15 @@
+function getZipcode(zipcode) {
+  console.log(zipcode);
+  $('#modal-zipcode').val(zipcode);
+}
+
+$(document).ready(function() {
+   // Event Handlers
+   $('#go-btn').click(function() {
+     var zipcode = $('#zipcode').val();
+
+     debugger
+
+     getZipcode(zipcode);
+   });
+});
