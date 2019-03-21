afd5272d29558e23babb69868c23f595962c8588
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-03-20 22:01:05 -0400

    work on spa form script.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -3,11 +3,33 @@ function getZipcode(zipcode) {
   $('#modal-zipcode').val(zipcode);
 }
 
-$(document).ready(function() {
-   // Event Handlers
-   $('#go-btn').click(function() {
-     var zipcode = $('#zipcode').val();
+function spaSection() {
+  $('.spa-section').one('keyup', function() {
+    // console.log('this: ', this);
+    // console.log($(this).attr('data-section'));
 
-     getZipcode(zipcode);
-   });
+    var dataSectionAttr = $(this).attr('data-section');
+    console.log(dataSectionAttr);
+
+    var dataNextConditional = $(`[data-section="${dataSectionAttr}"]`).next('.spa-section').length;
+
+    if (dataSectionAttr === "type") {
+      console.log('Cool')
+    } else {
+      if (dataNextConditional === 1) {
+        $(`[data-section="${dataSectionAttr}"]`).next('.spa-section').show();
+      }
+    }
+  });
+}
+
+$(document).ready(function() {
+  // Event Handlers
+  $('#go-btn').click(function() {
+    var zipcode = $('#zipcode').val();
+
+    getZipcode(zipcode);
+
+    spaSection();
+  });
 });
